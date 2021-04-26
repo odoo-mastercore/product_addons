75c4bc551061a6124a5355bd45095dd62d235a68
rhe-mastercore <rhe@mastercore.net>
2021-04-26 00:47:49 -0400

    [ADD] combo_product & ss_whatsapp

---
+++ combo_product/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2020-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    'name': 'Combo Product',
+    'summary': "Combo Product",
+    'description': """
+Combo Product.
+========================
+Create Combo Product . extension by SINAPSYS GLOBAL SA || MASTERCORE SAS
+    """,
+
+    'author': 'iPredict IT Solutions Pvt, SINAPSYS GLOBAL SA || MASTERCORE SAS',
+    'website': "http://sinapsys.global",
+    'category': 'Product',
+    'version': '13.0.0.1.0',
+    'depends': ['product', 'sale', 'sale_stock',],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/combo_product_view.xml',
+    ],
+
+    'license': "OPL-1",
+
+    'auto_install': False,
+    'installable': True,
+
+    'images': ['static/description/banner.png'],
+}
---
+++ combo_product/models/sale.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2020-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+from odoo import models, fields, api, _
+from odoo.tools.profiler import profile
+
+class ComboSaleOrder(models.Model):
+    _inherit = "sale.order"
+    """
+    @api.multi
+    @profile
+    def action_confirm(self):
+        res = super(ComboSaleOrder, self).action_confirm()
+        pickings_id = self.picking_ids
+        picking_lines = []
+        for line in self.order_line:
+            if line.product_id.combo_product_id:
+                for pack_id in line.product_id.combo_product_id:
+                    qty = pack_id.product_quantity * line.product_uom_qty
+                    picking_lines.append((0, 0, {
+                        'name': pack_id.product_id.name,
+                        'product_id': pack_id.product_id.id,
+                        'product_uom_qty': qty,
+                        'picking_id': pickings_id.id,
+                        'product_uom': pack_id.uom_id.id,
+                        'location_id': pickings_id.move_lines[0].location_id.id,
+                        'location_dest_id': pickings_id.move_lines[0].location_dest_id.id,
+                        'state': 'draft'}))
+        pickings_id.move_lines = picking_lines
+        #pickings_id.action_assign()
+        for move in pickings_id.move_lines:
+            move._action_assign()
+            move.filtered(lambda m: m.state in ['confirmed', 'waiting'])._force_assign()
+            move.filtered(lambda m: m.product_id.tracking == 'none')._action_done()
+        return res
+    """
+    def action_confirm(self):
+        self.ensure_one()
+        res = super(ComboSaleOrder, self).action_confirm()
+        #return res
+        #print([(x.name,x.state) for x in self.picking_ids])
+        pickings_ids = self.picking_ids.filtered(lambda m: m.state in [
+            'confirmed', 'waiting','done','assigned'])
+        #print([x.name for x in pickings_ids])
+        for pickings_id in pickings_ids:
+            picking_lines = []
+            for line in self.order_line:
+                if line.product_id.combo_product_id:
+                    for pack_id in line.product_id.combo_product_id:
+                        qty = round(pack_id.product_quantity * line.\
+                            product_uom_qty,0)
+                        picking_lines.append((0, 0, {
+                            'name': pack_id.product_id.name,
+                            'product_id': pack_id.product_id.id,
+                            'product_uom_qty': qty,
+                            #'quantity_done': qty,
+                            'picking_id': pickings_id.id,
+                            'product_uom': pack_id.uom_id.id,
+                            'location_id': pickings_id.move_lines[0].\
+                                location_id.id,
+                            'location_dest_id': pickings_id.move_lines[0].\
+                                location_dest_id.id,
+                            'state': 'draft'}))
+            pickings_id.move_lines = picking_lines
+            pickings_id.action_assign()
+            for move in pickings_id.move_lines:
+                move._action_assign()
+                move.filtered(lambda m: m.state in [
+                    'confirmed', 'waiting'])._force_assign()
+                move.filtered(
+                    lambda m: m.product_id.tracking == 'none')._action_done()
+        return res
