394ba106b6744b376c16fe9304557fb28b4051a4
Jose Bracho <brachojgbp@gmail.com>
2022-01-27 17:26:20 -0400

    [FIX] support ticket #12949

---
+++ sale_requisitions/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2020-Present.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    'name': 'Sale requisitions',
+    'version': '13.0.0.1',
+    'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
+    'website': 'https://sinapsys.global',
+    'license': 'AGPL-3',
+    'category': 'Sales',
+    'summary': 'Add to sale order on requisitions',
+    'depends': [
+        'sale',
+        'sale_stock',
+        'material_purchase_requisitions'
+    ],
+    'data': [
+        'views/sale_requisitions.xml',
+    ],
+    'installable': True,
+}
---
+++ sale_requisitions/models/sale_requisitions.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2020-Present.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+from odoo import models, fields, api, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleRequisitions(models.Model):
+
+    _inherit = 'material.purchase.requisition'
+
+    sale_order_id = fields.Many2one(
+        "sale.order",
+        string="Orden de venta",
+        domain="[('state', '=', 'draft')]"
+    )
+
+    @api.onchange('sale_order_id')
+    def _load_sale_order_line(self):
+        for rec in self:
+            if rec.sale_order_id.order_line:
+                order_lines = rec.sale_order_id.order_line
+                requisition_lines = []
+                for line in order_lines:
+                    requisition_lines.append([0, 0, {
+                        "product_id": line.product_id.id,
+                        "description": line.product_id.name,
+                        "qty": line.product_uom_qty,
+                        "uom": line.product_id.uom_id.id,
+                        "requisition_type": 'purchase'
+                    }])
+                rec.requisition_line_ids = requisition_lines
