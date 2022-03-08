9527a9193c1b6e76a89f65fecfdd9301c709e400
ejpr4 <elvispaez18@gmail.com>
2022-03-08 02:04:18 -0400

    [WIP] Parcial Module website_products_categories for filters products in website shop

---
+++ website_products_categories/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2022-Present.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    'name': 'Website products categories',
+    'version': '13.0.0.1',
+    'summary': 'Include additionals fields in /shop categories',
+    'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
+    'website': 'https://sinapsys.global',
+    'license': 'AGPL-3',
+    "category": "Product",
+    "depends": ["product", "website_sale", "product_vehicle_type", "droggol_theme_common", 'theme_prime'],
+    'data': [
+        'views/layout.xml'
+    ],
+    'installable': True,
+    "auto_install": False,
+}
