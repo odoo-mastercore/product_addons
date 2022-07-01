00116fc901b61b05b1f7cb3080577aa2330f64db
Kewitz Colina <kewitz.colina@gmail.com>
2022-06-30 22:41:29 -0400

    [ADD] Modulo invoice_report_friction

---
+++ invoice_report_friction/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2020-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+
+{
+    "name": "Invoice Report Friction",
+    "summary": "",
+    "version": "13.0.1.0.0",
+    "category": "Sales/CRM",
+    'website': "http://sinapsys.global",
+    'author': "SINAPSYS GLOBAL SA || MASTERCORE SAS",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        ],
+    "data": [
+        "templates/report_templates.xml",
+        "report/invoice_report_friction.xml"
+    ],
+}
