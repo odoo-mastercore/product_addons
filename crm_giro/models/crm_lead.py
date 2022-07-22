3aa1f60f1a4f55721f9c5849c4309cc8ea337fdf
Jose Bracho <brachojgbp@gmail.com>
2022-07-22 16:20:12 -0400

    [FIX] solved bug in crm_giro

--- crm_giro/models/crm_lead.py
+++ crm_giro/models/crm_lead.py
@@ -9,21 +9,45 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 import logging
+
 _logger = logging.getLogger(__name__)
+
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    def action_sale_quotations_new(self):
-        res = super(CrmLead, self).action_sale_quotations_new()
-        orders = self.mapped('order_ids').filtered(
-            lambda o: o.state in ('draft', 'sent', 'sale')
-        )
-        if orders:
-            raise UserError(_("No puede crear mas de un pedido de venta a una aportunidad"))
+    order = fields.Boolean(
+        string="Orden",
+        compute="_compute_quotation",
+        default=False
+    )
+    stage_id = fields.Many2one(
+        compute="_compute_stage",
+        index=True,
+        store=True,
+        readonly=True
+    )
+    state_picking = fields.Selection(
+        selection=[
+            ('assigned', 'PREPARADO'),
+            ('partial', 'PARCIALMENTE'),
+            ('done', 'REALIZADO'),
+            ('returns', 'DEVOLUCIONES'),
+            ('mediation', 'RECLAMOS')
+        ],
+        string="Estado del Picking"
+    )
 
-        return res
-
+    @api.depends('order_ids')
+    def _compute_quotation(self):
+        for lead in self:
+            orders = self.mapped('order_ids').filtered(
+                lambda o: o.state in ('draft', 'sent', 'sale')
+            )
+            if orders:
+                lead.update({'order': True})
+            else:
+                lead.update({'order': False})
 
     @api.depends('quotation_count')
     def _compute_stage(self):
@@ -35,7 +59,7 @@ class CrmLead(models.Model):
                 self.stage_id = stage.id
             elif len(rec.order_ids) == 1:
                 state_order = rec.order_ids[0].state
-                invoices = rec.order_ids[0].invoice_ids
+                # invoices = rec.order_ids[0].invoice_ids
                 if state_order == 'draft':
                     stage = self.env['crm.stage'].search(
                         [('name', '=', 'Presupuesto')]
@@ -56,36 +80,12 @@ class CrmLead(models.Model):
             elif len(rec.order_ids) >= 2:
                 pass
 
-
-    order = fields.Boolean(
-        string="Orden",
-        compute="_compute_quotation",
-        default=False
-    )
-
-    stage_id = fields.Many2one(
-        compute="_compute_stage",
-        index=True,
-        store=True,
-        readonly=True
-    )
-    state_picking = fields.Selection(
-        selection=[
-            ('assigned', 'PREPARADO'),
-            ('partial', 'PARCIALMENTE'),
-            ('done', 'REALIZADO'),
-            ('returns', 'DEVOLUCIONES'),
-            ('mediation', 'RECLAMOS')
-        ],
-        string="Estado del Picking"
-    )
-
-    @api.depends('order_ids')
-    def _compute_quotation(self):
+    def action_sale_quotations_new(self):
+        res = super(CrmLead, self).action_sale_quotations_new()
         orders = self.mapped('order_ids').filtered(
             lambda o: o.state in ('draft', 'sent', 'sale')
         )
         if orders:
-            self.order = True
-        else:
-            self.order = False
+            raise UserError(_("No puede crear mas de un pedido de venta a una aportunidad"))
+
+        return res
